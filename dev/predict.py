9a5399aa3fecefd81b219cdd0573252503c68489
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-10-25 20:16:44 +0530

    Fix Syntax error

--- dev/predict.py
+++ dev/predict.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 model = tf.keras.models.load_model("models/3x+3/linear.model")
 
-prediction[0] = model.predict([0,20,12,123,23,3,2.3])  # REMEMBER YOU'RE PASSING A LIST OF THINGS YOU WISH TO PREDICT
+prediction = model.predict([0,20,12,123,23,3,2.3])  # REMEMBER YOU'RE PASSING A LIST OF THINGS YOU WISH TO PREDICT
 # prediction[1] = model.predics
 print(prediction)
 
