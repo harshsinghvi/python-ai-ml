3ecdc0e883e31310cdf3c8f1545cc4b5bfb75001
Harsh Singhvi <harshsinghvi98@gmail.com>
2020-10-25 20:15:59 +0530

    Update dev

--- dev/predict.py
+++ dev/predict.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 
 
-model = tf.keras.models.load_model("linear.model")
+model = tf.keras.models.load_model("models/3x+3/linear.model")
 
-prediction = model.predict([0,20])  # REMEMBER YOU'RE PASSING A LIST OF THINGS YOU WISH TO PREDICT
-
+prediction[0] = model.predict([0,20,12,123,23,3,2.3])  # REMEMBER YOU'RE PASSING A LIST OF THINGS YOU WISH TO PREDICT
+# prediction[1] = model.predics
 print(prediction)
+
+
 # print(type(prediction))
 
 # print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
--- dev/train.py
+++ dev/train.py
@@ -7,15 +7,17 @@ def t(x):
 
 
 
-x = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], dtype=float)
+x = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,100,200,100000,500002,1233,3112], dtype=float)
 
 # x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
 y = x*3+3
 # print(x,y(x))
 
-
-if __name__ == '__main__':
+def main():
     neural_network_model = tf.keras.Sequential([tf.keras.layers.Dense(units=1,input_shape=[1])])
     neural_network_model.compile(optimizer='adam',loss='mse')
-    neural_network_model.fit(x,y,epochs=20000)
+    neural_network_model.fit(x,y,epochs=40000)
     neural_network_model.save("linear.model")
+
+if __name__ == '__main__':
+    main()
